a92a9cd7872d248f97b0c9f7c3f350da0eb8e91b
Rohan-Shakya <rohanshakya254@gmail.com>
2023-05-08 19:17:14 +0545

    deployed smart contract wallet

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.15",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
+
+export default config;
